16e1fca6d3d5a2328d0e091d2df9901adb5be400
Pedro Cleto <cl3t0.sh@gmail.com>
2023-08-29 16:51:45 -0300

    fix: redirect to main website in the navbar

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -25,7 +25,7 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex h-screen flex-col justify-between">
         <header className="flex items-center justify-between py-10">
           <div>
-            <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <Link href="https://gridoai.com" aria-label={siteMetadata.headerTitle} target="_self">
               <div className="flex items-center justify-between">
                 <div className="mr-3"></div>
                 {typeof siteMetadata.headerTitle[locale] === 'string' ? (
